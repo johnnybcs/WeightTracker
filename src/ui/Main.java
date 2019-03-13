f2f72d6a71b30c8cbae53efad6bad2106670a702
johnnyli <t3e2b@ugrad.cs.ubc.ca>
2019-03-12 23:34:52 -0700

    Added goal weight graph

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -19,11 +19,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import model.WeightRecord;
 
 public class Main extends Application {
     public static ObservableList<WeightRecord> weightRecords;
+    public static String weightGoal;
 
     private static final String TAB_1_NAME = "History";
     private static final String TAB_2_NAME = "Summary";
@@ -38,10 +40,13 @@ public class Main extends Application {
     private static final int ENTER_WEIGHT_TEXTFIELD_WIDTH = 124;
     private static final int NUMBER_OF_MONTHS_IN_YEAR = 12;
     public static final int ENTER_WEIGHT_BUTTON_HEIGHT = 34;
+    public static final int SET_WEIGHT_GOAL_TEXTFIELD_WIDTH = 70;
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        weightGoal = "";
 
         weightRecords = FXCollections.observableArrayList();
         for (int i = 1; i <= 12; i++) {
@@ -68,16 +73,42 @@ public class Main extends Application {
         BorderPane tab1BorderPane = new BorderPane();
         tab1BorderPane.setLeft(tableView);
 
-        Button setWeightGoalButton = new Button("Set Weight Goal");
-        TextField setWeightGoalTextField = new TextField();
-        setWeightGoalTextField.setMaxWidth(50);
+        Label setWeightGoalHeading = new Label("GOAL");
+        Label setWeightGoalValue = new Label(weightGoal);
 
-        VBox setWeightGoalVBox = new VBox(setWeightGoalButton, setWeightGoalTextField);
-        setWeightGoalVBox.setAlignment(Pos.CENTER);
+        setWeightGoalHeading.setFont(new Font("Arial bold", 20));
+        setWeightGoalHeading.setUnderline(true);
+
+        setWeightGoalValue.setFont(new Font("Arial bold", 40));
+
+        TextField setWeightGoalTextField = new TextField();
+        setWeightGoalTextField.setMaxWidth(SET_WEIGHT_GOAL_TEXTFIELD_WIDTH);
+
+        Button setWeightGoalButton = new Button("Set Weight Goal");
+        setWeightGoalButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Double.parseDouble(setWeightGoalTextField.getText());
+                    weightGoal = setWeightGoalTextField.getText();
+                    setWeightGoalValue.setText(weightGoal + " lbs");
+                } catch (Exception e) {
+                    System.out.println("BAD SET GOAL WEIGHT INPUT");
+                }
+            }
+        });
+
+        HBox setWeightGoalHBox = new HBox(setWeightGoalButton, setWeightGoalTextField);
+        setWeightGoalHBox.setAlignment(Pos.CENTER);
+
+
+
+        VBox setWeightGoalVBox = new VBox(setWeightGoalHeading, setWeightGoalValue, setWeightGoalHBox);
+        setWeightGoalVBox.setAlignment(Pos.TOP_CENTER);
+        setWeightGoalVBox.setMargin(setWeightGoalHeading, new Insets(25, 0, 0, 0));
+        setWeightGoalVBox.setMargin(setWeightGoalValue, new Insets(5, 0, 30, 0));
 
         tab1BorderPane.setCenter(setWeightGoalVBox);
-//        tab1BorderPane.setAlignment(setWeightGoalVBox, Pos.BOTTOM_CENTER);
-
 
         TextField enterWeightTextField = new TextField();
         enterWeightTextField.setPrefWidth(ENTER_WEIGHT_TEXTFIELD_WIDTH);
@@ -160,7 +191,18 @@ public class Main extends Application {
                         }
                     }
 
+                    XYChart.Series dataSeries2 = new XYChart.Series();
+                    dataSeries2.setName("Goal");
+                    if (weightRecords.get(0).getWeight() != "" && weightGoal != "") {
+                        dataSeries2.getData().add(new XYChart.Data(1,
+                                Double.parseDouble(weightRecords.get(0).getWeight())));
+
+                        dataSeries2.getData().add(new XYChart.Data(NUMBER_OF_MONTHS_IN_YEAR,
+                                Double.parseDouble(weightGoal)));
+                    }
+
                     lineChart.getData().add(dataSeries1);
+                    lineChart.getData().add(dataSeries2);
 
                     tab3.setContent(lineChart);
                 }
