39b7bf3d9fdd40b2acdcc205faa8686c22918f67
johnnyli <t3e2b@ugrad.cs.ubc.ca>
2019-03-21 10:44:37 -0700

    Made chat bot more robust

--- src/model/Tab4.java
+++ src/model/Tab4.java
@@ -45,6 +45,7 @@ public class Tab4 {
                     textArea = new TextArea();
                     textArea.setEditable(false);
                     textArea.setPrefHeight(TEXT_AREA_HEIGHT);
+                    textArea.setWrapText(true);
                     textArea.setText(COACH_NAME + "What can I do for you today?\n");
 
                     textField = new TextField();
@@ -85,14 +86,13 @@ public class Tab4 {
     }
 
     private void reply() {
+        randomNumberGenerator = new Random();
+        int randomNumber = randomNumberGenerator.nextInt(20) + 1;
         if (userInput.contains("hi") || userInput.contains("hello")
-                || userInput.contains("hey") || userInput.contains("how are you")
-                || userInput.contains("how's it going") || userInput.contains("what's up")
-                || userInput.contains("good morning") || userInput.contains("good afternoon")
-                || userInput.contains("good evening") || userInput.contains("nice to meet")) {
-
-            randomNumberGenerator = new Random();
-            int randomNumber = randomNumberGenerator.nextInt(10) + 1;
+                    || userInput.contains("hey") || userInput.contains("how are you")
+                    || userInput.contains("how's it going") || userInput.contains("what's up")
+                    || userInput.contains("good morning") || userInput.contains("good afternoon")
+                    || userInput.contains("good evening") || userInput.contains("nice to meet")) {
 
             switch (randomNumber) {
                 case (1):
@@ -115,9 +115,55 @@ public class Tab4 {
                     break;
                 case (7):
                     coachResponse("Sup!");
+                    break;
                 default:
                     coachResponse("Hello!");
             }
+        } else if (userInput.contains("bye") || userInput.contains("goodbye")
+                    || userInput.contains("farewell") || userInput.contains("later")
+                    || userInput.contains("so long") || userInput.contains("see ya")) {
+
+            switch (randomNumber) {
+                case (1):
+                    coachResponse("See ya!");
+                    break;
+                case (2):
+                    coachResponse("Goodbye!");
+                    break;
+                case (3):
+                    coachResponse("Farewell!");
+                    break;
+                case (4):
+                    coachResponse("Take care!");
+                    break;
+                case (5):
+                    coachResponse("Bye bye!");
+                    break;
+                case (6):
+                    coachResponse("See you later!");
+                    break;
+                case (7):
+                    coachResponse("So long!");
+                    break;
+                default:
+                    coachResponse("Have a good one!");
+            }
+        } else if (userInput.contains("thank")) {
+
+                switch (randomNumber) {
+                    case (1):
+                        coachResponse("No problem!");
+                        break;
+                    case (2):
+                        coachResponse("You're welcome.");
+                        break;
+                    case (3):
+                        coachResponse("My pleasure.");
+                        break;
+                    default:
+                        coachResponse("I'm happy to help.");
+                }
+
         } else if (userInput.contains("starting weight")
                 || userInput.contains("start weight")) {
             coachResponse("You weighed " + startingWeight
@@ -136,11 +182,64 @@ public class Tab4 {
                 || userInput.contains("have left")) {
             coachResponse("Just " + remainingWeight + " pounds to go!");
         } else if (userInput.contains("i lost")
-                || userInput.contains("i lose")
+                || userInput.contains("how much weight did i lose")
                 || userInput.contains("weight loss")) {
             coachResponse("You lost " + lostWeight + " pounds so far.");
-        } else {
+        } else if (userInput.contains("how do")
+                || userInput.contains("how to")
+                || userInput.contains("do to")) {
             coachResponse("Diet and exercise!");
+        } else {
+
+            switch (randomNumber) {
+                case (1):
+                    coachResponse("Only you can change your life. No one can do it for you.");
+                    break;
+                case (2):
+                    coachResponse("Doubt kills more dreams than failure ever will.");
+                    break;
+                case (3):
+                    coachResponse("The struggle you are in today is developing the strength you need for tomorrow.");
+                    break;
+                case (4):
+                    coachResponse("The road may be bumpy, but stay committed to the process.");
+                    break;
+                case (5):
+                    coachResponse("Will is a skill.");
+                    break;
+                case (6):
+                    coachResponse("Stressed spelled backwards is desserts. Coincidence? I think not!");
+                    break;
+                case (7):
+                    coachResponse("Someone busier than you is running right now.");
+                    break;
+                case (8):
+                    coachResponse("You can't cross the sea merely by standing and staring at the water.");
+                    break;
+                case (9):
+                    coachResponse("Strive for progress, not perfection.");
+                    break;
+                case (10):
+                    coachResponse("Make your body the sexiest outfit you own.");
+                    break;
+                case (11):
+                    coachResponse("It won't be easy but it'll be worth it.");
+                    break;
+                case (12):
+                    coachResponse("Never let a stumble in the road be the end of your journey.");
+                    break;
+                case (13):
+                    coachResponse("Look back at where you came from and let yourself feel proud about your progress.");
+                    break;
+                case (14):
+                    coachResponse("Eat less sugar. You're sweet enough already.");
+                    break;
+                case (15):
+                    coachResponse("When you feel like quitting, think about why you started.");
+                    break;
+                default:
+                    coachResponse("Never, never, never give up.");
+            }
         }
     }
 }
